00d185dec1114117cab9c991fd624248e57826f7
chriseikelboom <ceikelboom@sp.nl>
2016-11-03 12:41:11 +0100

    Added Aftrap v2. Added Lees-ook

---
+++ js/spruit_nzf.js
@@ -0,0 +1,17 @@
+(function ($) {
+  Drupal.behaviors.NZFanimate = {
+    attach: function (context, settings) {
+
+      setTimeout(function(){
+        $("#nzf-header-aftrap").addClass("nzf-header-aftrap-show");
+      }, 1000);
+
+      $("#nzf-header-aftrap").click(function(){
+        window.location.href = "/26-november";
+      });
+
+    }
+  }
+
+})(jQuery);
+
