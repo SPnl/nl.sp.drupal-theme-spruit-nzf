f8c7d83f82e3739c124244edf3b350e5e5aff58f
chriseikelboom <ceikelboom@sp.nl>
2021-02-02 13:24:37 +0100

    Futher cleaning. Fixed grid layout.

--- templates/page--node.tpl.php
+++ templates/page--node.tpl.php
@@ -5,9 +5,10 @@
  * Default theme implementation to display a single Drupal page.
  */
 ?>
-  <div class="page-wrapper"><div class="page">
-    <header class="site-header">
+<div class="page-wrapper">
+  <div class="page">
 
+    <header class="site-header">
       <div class="site-branding">
       <?php if ($logo): ?>
         <div class="site-logo">
@@ -31,39 +32,26 @@
       <?php endif; ?>
     </header>
 
-     <?php if ($page['search']): ?>
-      <div class="site-search">
-        <?php print render($page['search']); ?>
-      </div>
-    <?php endif; ?>
-
-    <?php print $messages; ?>
-
-      <div class="primary-content">
-        <?php if ($title): ?>
-        <header class="content-header">
-          <?php print render($title_prefix); ?>
-          <h1 class="title"><?php print $title; ?></h1>
-          <?php print render($title_suffix); ?>
-        </header>
-        <?php endif; ?>
-          <?php print render($tabs); ?>
-          <?php print render($page['help']); ?>
-
-          <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-
-        <a id="primary-content"></a>
-        <main class="content"><?php print render($page['content']); ?></main>
-      </div>
-
-      <?php if ($page['secondary']): ?>
-        <div class="secondary-content">
-          <?php print render($page['secondary']); ?>
-        </div>
+    <div class="primary-content">
+      <?php if ($title): ?>
+      <header class="content-header">
+        <?php print render($title_prefix); ?>
+        <h1 class="title"><?php print $title; ?></h1>
+        <?php print render($title_suffix); ?>
+      </header>
       <?php endif; ?>
-    </main>
+      <?php print render($tabs); ?>
+      <?php print render($page['help']); ?>
+
+      <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+      <?php print $messages; ?>
+      <a id="primary-content"></a>
+      <main class="content"><?php print render($page['content']); ?></main>
+    </div>
 
     <footer class="site-footer">
       <?php print render($page['footer']); ?>
     </footer>
-  </div></div>
+
+  </div>
+</div>
